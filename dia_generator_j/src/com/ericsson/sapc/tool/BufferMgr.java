c4c6b95c5c65152c161063e4446eafb71bab52e4
kekenow <zhongke@live.com>
2014-12-12 15:40:06 +0800

    Format code style

--- dia_generator_j/src/com/ericsson/sapc/tool/BufferMgr.java
+++ dia_generator_j/src/com/ericsson/sapc/tool/BufferMgr.java
@@ -15,144 +15,144 @@ import com.ericsson.sapc.tool.ConstantType.EVENT_TYPE;
 import com.ericsson.sapc.tool.ConstantType.MSG_FLOW;
 
 public class BufferMgr {
-//	private static String PATTERN_EVENT = "t_[a-z]*_[a-z]*_event";
-	private static String PATTERN_EVENT = "t_[3,a-z,/_]*_event";
-//	private static String PATTERN_EVENT_SY = "t_[a-z]*_[a-z]*_[a-z]*_event";
-	private static String PATTERN_FUNCTION = "f_runEvent";
-	private static String PATTERN_EVENT_FLOW = "eventFlow";
-	// vl_gxa_Event.eventFlow := REQUEST;
+    // private static String PATTERN_EVENT = "t_[a-z]*_[a-z]*_event";
+    private static String PATTERN_EVENT = "t_[3,a-z,/_]*_event";
+    // private static String PATTERN_EVENT_SY = "t_[a-z]*_[a-z]*_[a-z]*_event";
+    private static String PATTERN_FUNCTION = "f_runEvent";
+    private static String PATTERN_EVENT_FLOW = "eventFlow";
+    // vl_gxa_Event.eventFlow := REQUEST;
 
-	private List<Event> events = new LinkedList<Event>();
-	private Set<String> nodeSet = new HashSet<String>();
-	private List<String> nodeList = new LinkedList<String>();
+    private List<Event> events = new LinkedList<Event>();
+    private Set<String> nodeSet = new HashSet<String>();
+    private List<String> nodeList = new LinkedList<String>();
 
-	public void readInputFromFile(String fileName) {
+    public void readInputFromFile(String fileName) {
 
-		BufferedReader bufferedReader = null;
-		try {
-			bufferedReader = new BufferedReader(new FileReader(fileName));
-			String line = null;
-			Event event = new Event();
-			boolean isSameFlow = true;
-			int sequenceNumber = 0;
-			Matcher matcher_event = null;
-			Pattern pattern_event = Pattern.compile(PATTERN_EVENT);
-			while (bufferedReader.ready()) {
-				line = bufferedReader.readLine();
-				event.setNodeSeqence(sequenceNumber);
-				if (!isSameFlow) {
-					event = new Event();
-				}
-				if (null != line && !"".equals(line)) {
-					// Get event list from input by reqex
-					matcher_event = pattern_event.matcher(line);
-					if (matcher_event.find()) {
-						event.setEventType(matcher_event.group(0));
-						getEventInfo(line, event);
-						isSameFlow = true;
-					} else if (line.contains(PATTERN_EVENT_FLOW)) {
-						// Get event flow if has
-						String eventFlow = line.split("=")[1].split(";")[0].trim();
-						event.setEventFlow(eventFlow);
-						isSameFlow = true;
-					} else if (line.contains(PATTERN_FUNCTION)) {
-						// Get node list from input
-						String nodeName = line.split("\\]")[1].split("\\[")[1];
-						event.setNodeName(nodeName);
-						nodeSet.add(nodeName);
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line = null;
+            Event event = new Event();
+            boolean isSameFlow = true;
+            int sequenceNumber = 0;
+            Matcher matcher_event = null;
+            Pattern pattern_event = Pattern.compile(PATTERN_EVENT);
+            while (bufferedReader.ready()) {
+                line = bufferedReader.readLine();
+                event.setNodeSeqence(sequenceNumber);
+                if (!isSameFlow) {
+                    event = new Event();
+                }
+                if (null != line && !"".equals(line)) {
+                    // Get event list from input by reqex
+                    matcher_event = pattern_event.matcher(line);
+                    if (matcher_event.find()) {
+                        event.setEventType(matcher_event.group(0));
+                        getEventInfo(line, event);
+                        isSameFlow = true;
+                    } else if (line.contains(PATTERN_EVENT_FLOW)) {
+                        // Get event flow if has
+                        String eventFlow = line.split("=")[1].split(";")[0].trim();
+                        event.setEventFlow(eventFlow);
+                        isSameFlow = true;
+                    } else if (line.contains(PATTERN_FUNCTION)) {
+                        // Get node list from input
+                        String nodeName = line.split("\\]")[1].split("\\[")[1];
+                        event.setNodeName(nodeName);
+                        nodeSet.add(nodeName);
 
-						// Create a map to contain the node list and node message
-						events.add(event);
-						isSameFlow = false;
-						++sequenceNumber;
-					}
-				}
-			}
-			// Put this info into a list
-			
-			// Get node list from events
-			for (Event e : events) {
-				if (!nodeList.contains(e.getNodeName())){
-					nodeList.add(e.getNodeName());
-				}
-			}
-			
-			// Add SAPC node into the second position
-			nodeList.add(1, "SAPC");
-			for (int i = 0; i < nodeList.size(); ++i) {
-				System.out.println(nodeList.get(i));
-			}
+                        // Create a map to contain the node list and node message
+                        events.add(event);
+                        isSameFlow = false;
+                        ++sequenceNumber;
+                    }
+                }
+            }
+            // Put this info into a list
 
-			System.out.println("----------------------------------------");
+            // Get node list from events
+            for (Event e : events) {
+                if (!nodeList.contains(e.getNodeName())) {
+                    nodeList.add(e.getNodeName());
+                }
+            }
 
-		} catch (IOException e) {
-			System.out.println("File does not exist");
-			e.printStackTrace();
-		} finally {
-			try {
-				if (null != bufferedReader) {
-					bufferedReader.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+            // Add SAPC node into the second position
+            nodeList.add(1, "SAPC");
+            for (int i = 0; i < nodeList.size(); ++i) {
+                System.out.println(nodeList.get(i));
+            }
 
-	}
+            System.out.println("----------------------------------------");
 
-	private void getEventInfo(String line, Event event) {
-		String release;
-		String requestType;
-		String eventType = event.getEventType();
-		release = line.split(",")[2];
-		event.setRelease(release);
-		// Get the request type if the event is ccr event from Gx, Gxa, Sx
-		if (EVENT_TYPE.GX_CCR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.GXA_CCR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.SX_CCR_EVENT.toString().equals(eventType)) {
-			requestType = line.split(",")[3].split("\\)")[0].trim();
-			event.setRequestType(requestType);
-		}
-	}
+        } catch (IOException e) {
+            System.out.println("File does not exist");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != bufferedReader) {
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-	public void showDiagramFromBuffer() {
+    }
 
-		Diagram.showHeaderLine(nodeList);
-		Diagram.showCommonLine(Diagram.COMMON.FIRST, nodeList);
-		Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
+    private void getEventInfo(String line, Event event) {
+        String release;
+        String requestType;
+        String eventType = event.getEventType();
+        release = line.split(",")[2];
+        event.setRelease(release);
+        // Get the request type if the event is ccr event from Gx, Gxa, Sx
+        if (EVENT_TYPE.GX_CCR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.GXA_CCR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.SX_CCR_EVENT.toString().equals(eventType)) {
+            requestType = line.split(",")[3].split("\\)")[0].trim();
+            event.setRequestType(requestType);
+        }
+    }
 
-		Iterator<Event> iter = events.iterator();
-		String eventType = null;
-		String requestType = null;
-		while (iter.hasNext()) {
-			Event event = (Event) iter.next();
-			String eventFlow = event.getEventFlow();
-			if (null == eventFlow) {
-				event.setEventFlow(MSG_FLOW.REQUEST.toString());
-				Diagram.showMessageLine(event, nodeList);
-				Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
-				event.setEventFlow(MSG_FLOW.ANSWER.toString());
-				Diagram.showMessageLine(event, nodeList);
-			} else {
-				if (MSG_FLOW.REQUEST.toString().equals(eventFlow)) {
-					event.setEventFlow(MSG_FLOW.REQUEST.toString());
-					Diagram.showMessageLine(event, nodeList);
-					eventType = event.getEventType();
-					requestType = event.getRequestType();
-				} else {
-					// Due to no event initialization for ANSWER event flow
-					// So reuse the REQUEST info above
-					event.setEventFlow(MSG_FLOW.ANSWER.toString());
-					event.setEventType(eventType);
-					event.setRequestType(requestType);
-					Diagram.showMessageLine(event, nodeList);
-				}
-				Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
-			}
+    public void showDiagramFromBuffer() {
 
-		}
-		Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
-		Diagram.showCommonLine(Diagram.COMMON.LAST, nodeList);
-	}
+        Diagram.showHeaderLine(nodeList);
+        Diagram.showCommonLine(Diagram.COMMON.FIRST, nodeList);
+        Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
+
+        Iterator<Event> iter = events.iterator();
+        String eventType = null;
+        String requestType = null;
+        while (iter.hasNext()) {
+            Event event = (Event) iter.next();
+            String eventFlow = event.getEventFlow();
+            if (null == eventFlow) {
+                event.setEventFlow(MSG_FLOW.REQUEST.toString());
+                Diagram.showMessageLine(event, nodeList);
+                Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
+                event.setEventFlow(MSG_FLOW.ANSWER.toString());
+                Diagram.showMessageLine(event, nodeList);
+            } else {
+                if (MSG_FLOW.REQUEST.toString().equals(eventFlow)) {
+                    event.setEventFlow(MSG_FLOW.REQUEST.toString());
+                    Diagram.showMessageLine(event, nodeList);
+                    eventType = event.getEventType();
+                    requestType = event.getRequestType();
+                } else {
+                    // Due to no event initialization for ANSWER event flow
+                    // So reuse the REQUEST info above
+                    event.setEventFlow(MSG_FLOW.ANSWER.toString());
+                    event.setEventType(eventType);
+                    event.setRequestType(requestType);
+                    Diagram.showMessageLine(event, nodeList);
+                }
+                Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
+            }
+
+        }
+        Diagram.showCommonLine(Diagram.COMMON.MIDDLE, nodeList);
+        Diagram.showCommonLine(Diagram.COMMON.LAST, nodeList);
+    }
 
 }
--- dia_generator_j/src/com/ericsson/sapc/tool/Diagram.java
+++ dia_generator_j/src/com/ericsson/sapc/tool/Diagram.java
@@ -7,235 +7,215 @@ import com.ericsson.sapc.tool.ConstantType.MSG_FLOW;
 import com.ericsson.sapc.tool.ConstantType.MSG_TYPE;
 import com.ericsson.sapc.tool.ConstantType.REQUEST_TYPE;
 
-//*         SGSN_MME                     SAPC                        GGSN
-//*        ________                    ________                    ________
-//*       |        |                  |        |                  |        |
-//*       |        |                  |        |                  |        |
-//*       |        |                  |        |                  |        |
-//*       |        | (1) CCR-I        |        |                  |        |
-//*       |        | ---------------> |        |                  |        |	
-//*       |        |                  |        |                  |        |
-//*       |________|                  |________|                  |________|
 
 public class Diagram {
-	public static String BEGIN = "*       ";
-	public static String BLANK = "                  ";
-	public static String HEADER = " ________ ";
-	public static String MIDDLE = "|        |";
-	public static String BOTTOM = "|________|";
-	public static String LEFT = " <--------------- ";
-	public static String RIGHT = " ---------------> ";
+    public static String BEGIN = "*       ";
+    public static String BLANK = "                  ";
+    public static String HEADER = " ________ ";
+    public static String MIDDLE = "|        |";
+    public static String BOTTOM = "|________|";
+    public static String LEFT = " <--------------- ";
+    public static String RIGHT = " ---------------> ";
 
-	enum COMMON {
-		FIRST, MIDDLE, LAST
-	}
+    enum COMMON {
+        FIRST, MIDDLE, LAST
+    }
 
-//	*        ________                    ________                    ________ 
-//	*       |        |                  |        |                  |        |
-//	*       |        |                  |        |                  |        |
-//	*       |________|                  |________|                  |________|
-	public static void showCommonLine(COMMON type, List<String> nodeList) {
-		// Take the SAPC node into the second position
-		StringBuffer lineMessage = new StringBuffer();
-		lineMessage.append(Diagram.BEGIN);
+    public static void showCommonLine(COMMON type, List<String> nodeList) {
+        // Take the SAPC node into the second position
+        StringBuffer lineMessage = new StringBuffer();
+        lineMessage.append(Diagram.BEGIN);
 
-		for (int i = 0; i < nodeList.size(); ++i) {
-			if (Diagram.COMMON.FIRST == type) {
-				lineMessage.append(Diagram.HEADER);
-			} else if (Diagram.COMMON.MIDDLE == type) {
-				lineMessage.append(Diagram.MIDDLE);
-			} else if (Diagram.COMMON.LAST == type) {
-				lineMessage.append(Diagram.BOTTOM);
-			}
-			if (i != nodeList.size() - 1) {
-				lineMessage.append(Diagram.BLANK);
-			}
-		}
-		System.out.println(lineMessage);
-	}
+        for (int i = 0; i < nodeList.size(); ++i) {
+            if (Diagram.COMMON.FIRST == type) {
+                lineMessage.append(Diagram.HEADER);
+            } else if (Diagram.COMMON.MIDDLE == type) {
+                lineMessage.append(Diagram.MIDDLE);
+            } else if (Diagram.COMMON.LAST == type) {
+                lineMessage.append(Diagram.BOTTOM);
+            }
+            if (i != nodeList.size() - 1) {
+                lineMessage.append(Diagram.BLANK);
+            }
+        }
+        System.out.println(lineMessage);
+    }
 
-//*        |        |  (1) CCR-I       |        |                  |        |
-//*        |        | ---------------> |        |                  |        |
-	public static void showMessageLine(Event event, List<String> nodeList) {
-		StringBuffer lineMessage = new StringBuffer();
-		StringBuffer lineArrow = new StringBuffer();
-		lineMessage.append(Diagram.BEGIN);
-		lineArrow.append(Diagram.BEGIN);
+    public static void showMessageLine(Event event, List<String> nodeList) {
+        StringBuffer lineMessage = new StringBuffer();
+        StringBuffer lineArrow = new StringBuffer();
+        lineMessage.append(Diagram.BEGIN);
+        lineArrow.append(Diagram.BEGIN);
 
-		// Get node position
-		boolean needBlank = true;
-		int position = nodeList.indexOf(event.getNodeName());
-		event.setNodePosition(position);
+        // Get node position
+        boolean needBlank = true;
+        int position = nodeList.indexOf(event.getNodeName());
+        event.setNodePosition(position);
 
-		for (int i = 0; i < nodeList.size(); ++i) {
-			if (i != 2 || position != 2) {
-				lineMessage.append(Diagram.MIDDLE);
-				lineArrow.append(Diagram.MIDDLE);
-				needBlank = true;
-			}
-			if (i == position) {
-				// Check the message type
-				if (i >= 2) {
-					lineMessage.delete(lineMessage.lastIndexOf(Diagram.BLANK),
-							lineMessage.length());
-					lineArrow.delete(lineArrow.lastIndexOf(Diagram.BLANK),
-							lineArrow.length());
-					assembleMessage(lineMessage, lineArrow, event);
-					lineMessage.append(Diagram.MIDDLE);
-					lineArrow.append(Diagram.MIDDLE);
-				} else {
-					assembleMessage(lineMessage, lineArrow, event);
-					needBlank = false;
-				}
-			}
+        for (int i = 0; i < nodeList.size(); ++i) {
+            if (i != 2 || position != 2) {
+                lineMessage.append(Diagram.MIDDLE);
+                lineArrow.append(Diagram.MIDDLE);
+                needBlank = true;
+            }
+            if (i == position) {
+                // Check the message type
+                if (i >= 2) {
+                    lineMessage.delete(lineMessage.lastIndexOf(Diagram.BLANK), lineMessage.length());
+                    lineArrow.delete(lineArrow.lastIndexOf(Diagram.BLANK), lineArrow.length());
+                    assembleMessage(lineMessage, lineArrow, event);
+                    lineMessage.append(Diagram.MIDDLE);
+                    lineArrow.append(Diagram.MIDDLE);
+                } else {
+                    assembleMessage(lineMessage, lineArrow, event);
+                    needBlank = false;
+                }
+            }
 
-			if (i < nodeList.size() - 1 && needBlank) {
-				lineMessage.append(Diagram.BLANK);
-				lineArrow.append(Diagram.BLANK);
-			}
-			needBlank = true;
-		}
-		System.out.println(lineMessage.toString());
-		System.out.println(lineArrow.toString());
-	}
+            if (i < nodeList.size() - 1 && needBlank) {
+                lineMessage.append(Diagram.BLANK);
+                lineArrow.append(Diagram.BLANK);
+            }
+            needBlank = true;
+        }
+        System.out.println(lineMessage.toString());
+        System.out.println(lineArrow.toString());
+    }
 
-//	*        SGSN_MME                      SAPC                        GGSN   
-	public static void showHeaderLine(List<String> nodeList) {
-		StringBuffer lineHeader = new StringBuffer();
-		lineHeader.append(Diagram.BEGIN);
-		String nodeName = null;
-		for (int i = 0; i < nodeList.size(); ++i) {
-			nodeName = nodeList.get(i).toString();
-			// fill more blank to the line until the Diagram.MIDDLE
-			int blankSpace = HEADER.length() - nodeName.length();
-			int nextBlank = 0;
-			if (blankSpace % 2 == 0) {
-				nextBlank = blankSpace / 2;
-			} else {
-				nextBlank = blankSpace / 2 + 1;
-			}
-			for (int j = 0; j < blankSpace / 2; ++j) {
-				lineHeader.append(" ");
-			}
-			lineHeader.append(nodeName);
-			for (int j = 0; j < nextBlank; ++j) {
-				lineHeader.append(" ");
-			}
-			if (i < nodeList.size() - 1) {
-				lineHeader.append(Diagram.BLANK);
-			}
-		}
-		System.out.println(lineHeader.toString());
-	}
+    // * SGSN_MME SAPC GGSN
+    public static void showHeaderLine(List<String> nodeList) {
+        StringBuffer lineHeader = new StringBuffer();
+        lineHeader.append(Diagram.BEGIN);
+        String nodeName = null;
+        for (int i = 0; i < nodeList.size(); ++i) {
+            nodeName = nodeList.get(i).toString();
+            // fill more blank to the line until the Diagram.MIDDLE
+            int blankSpace = HEADER.length() - nodeName.length();
+            int nextBlank = 0;
+            if (blankSpace % 2 == 0) {
+                nextBlank = blankSpace / 2;
+            } else {
+                nextBlank = blankSpace / 2 + 1;
+            }
+            for (int j = 0; j < blankSpace / 2; ++j) {
+                lineHeader.append(" ");
+            }
+            lineHeader.append(nodeName);
+            for (int j = 0; j < nextBlank; ++j) {
+                lineHeader.append(" ");
+            }
+            if (i < nodeList.size() - 1) {
+                lineHeader.append(Diagram.BLANK);
+            }
+        }
+        System.out.println(lineHeader.toString());
+    }
 
-	private static void assembleMessage(StringBuffer lineMessage,
-			StringBuffer lineArrow, Event event) {
-		String eventType = event.getEventType();
-		StringBuffer msg = new StringBuffer();
-		String flow = event.getEventFlow();
+    private static void assembleMessage(StringBuffer lineMessage, StringBuffer lineArrow, Event event) {
+        String eventType = event.getEventType();
+        StringBuffer msg = new StringBuffer();
+        String flow = event.getEventFlow();
 
-		msg.append("  (");
-		msg.append(event.getNodeSeqence());
-		msg.append(") ");
-		if (EVENT_TYPE.SX_CCR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.GX_CCR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.GXA_CCR_EVENT.toString().equals(eventType)) {
-			String requestType = event.getRequestType();
-			event.setSapcInitialized(false);
-			if (REQUEST_TYPE.INITIAL_REQUEST.toString().equals(requestType)) {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					msg.append(MSG_TYPE.CCR_I);
-				} else {
-					msg.append(MSG_TYPE.CCA_I);
-				}
-			} else if (REQUEST_TYPE.UPDATE_REQUEST.toString().equals(requestType)) {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					msg.append(MSG_TYPE.CCR_U);
-				} else {
-					msg.append(MSG_TYPE.CCA_U);
-				}
-			} else if (REQUEST_TYPE.TERMINATION_REQUEST.toString().equals(requestType)) {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					msg.append(MSG_TYPE.CCR_T);
-				} else {
-					msg.append(MSG_TYPE.CCA_T);
-				}
-			}
-		} else if (EVENT_TYPE.SX_RAR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.GX_RAR_EVENT.toString().equals(eventType)
-				|| EVENT_TYPE.GXA_RAR_EVENT.toString().equals(eventType)
-		) {
-			event.setSapcInitialized(true);
-			if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-				msg.append(MSG_TYPE.RAR);
-			} else {
-				msg.append(MSG_TYPE.RAA);
-			}
-		} else if (EVENT_TYPE.SY_SLR_EVENT.toString().equals(eventType)) {
-			event.setSapcInitialized(true);
-			if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-				msg.append(MSG_TYPE.SLR);
-			} else {
-				msg.append(MSG_TYPE.SLA);
-			}
-		} else if (EVENT_TYPE.SY_SNR_EVENT.toString().equals(eventType)) {
-			event.setSapcInitialized(false);
-			if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-				msg.append(MSG_TYPE.SNR);
-			} else {
-				msg.append(MSG_TYPE.SNA);
-			}
-		} else if (EVENT_TYPE.SY_STR_EVENT.toString().equals(eventType)) {
-			event.setSapcInitialized(false);
-			if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-				msg.append(MSG_TYPE.STR);
-			} else {
-				msg.append(MSG_TYPE.STA);
-			}
-		}
+        msg.append("  (");
+        msg.append(event.getNodeSeqence());
+        msg.append(") ");
+        if (EVENT_TYPE.SX_CCR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.GX_CCR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.GXA_CCR_EVENT.toString().equals(eventType)) {
+            String requestType = event.getRequestType();
+            event.setSapcInitialized(false);
+            if (REQUEST_TYPE.INITIAL_REQUEST.toString().equals(requestType)) {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    msg.append(MSG_TYPE.CCR_I);
+                } else {
+                    msg.append(MSG_TYPE.CCA_I);
+                }
+            } else if (REQUEST_TYPE.UPDATE_REQUEST.toString().equals(requestType)) {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    msg.append(MSG_TYPE.CCR_U);
+                } else {
+                    msg.append(MSG_TYPE.CCA_U);
+                }
+            } else if (REQUEST_TYPE.TERMINATION_REQUEST.toString().equals(requestType)) {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    msg.append(MSG_TYPE.CCR_T);
+                } else {
+                    msg.append(MSG_TYPE.CCA_T);
+                }
+            }
+        } else if (EVENT_TYPE.SX_RAR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.GX_RAR_EVENT.toString().equals(eventType)
+                || EVENT_TYPE.GXA_RAR_EVENT.toString().equals(eventType)) {
+            event.setSapcInitialized(true);
+            if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                msg.append(MSG_TYPE.RAR);
+            } else {
+                msg.append(MSG_TYPE.RAA);
+            }
+        } else if (EVENT_TYPE.SY_SLR_EVENT.toString().equals(eventType)) {
+            event.setSapcInitialized(true);
+            if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                msg.append(MSG_TYPE.SLR);
+            } else {
+                msg.append(MSG_TYPE.SLA);
+            }
+        } else if (EVENT_TYPE.SY_SNR_EVENT.toString().equals(eventType)) {
+            event.setSapcInitialized(false);
+            if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                msg.append(MSG_TYPE.SNR);
+            } else {
+                msg.append(MSG_TYPE.SNA);
+            }
+        } else if (EVENT_TYPE.SY_STR_EVENT.toString().equals(eventType)) {
+            event.setSapcInitialized(false);
+            if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                msg.append(MSG_TYPE.STR);
+            } else {
+                msg.append(MSG_TYPE.STA);
+            }
+        }
 
-		lineMessage.append(msg.toString());
-		showFlow(lineArrow, event, flow);
-		
-		// fill more blank to the line until the Diagram.MIDDLE
-		for (int i = 0; i < BLANK.length() - msg.length(); ++i) {
-			lineMessage.append(" ");
-		}
-	}
+        lineMessage.append(msg.toString());
+        showFlow(lineArrow, event, flow);
 
-	private static void showFlow(StringBuffer lineArrow, Event event,
-			String flow) {
-		if (!event.isSapcInitialized()){
-			if (event.getNodePosition() < 1) {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					lineArrow.append(RIGHT);
-				} else {
-					lineArrow.append(LEFT);
-				}
-			} else {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					lineArrow.append(LEFT);
-				} else {
-					lineArrow.append(RIGHT);
-				}
-				
-			}
-		} else {
-			if (event.getNodePosition() < 1) {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					lineArrow.append(LEFT);
-				} else {
-					lineArrow.append(RIGHT);
-				}
-			} else {
-				if (MSG_FLOW.REQUEST.toString().equals(flow)) {
-					lineArrow.append(RIGHT);
-				} else {
-					lineArrow.append(LEFT);
-				}
-				
-			}
-		}
-	}
+        // fill more blank to the line until the Diagram.MIDDLE
+        for (int i = 0; i < BLANK.length() - msg.length(); ++i) {
+            lineMessage.append(" ");
+        }
+    }
+
+    private static void showFlow(StringBuffer lineArrow, Event event, String flow) {
+        if (!event.isSapcInitialized()) {
+            if (event.getNodePosition() < 1) {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    lineArrow.append(RIGHT);
+                } else {
+                    lineArrow.append(LEFT);
+                }
+            } else {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    lineArrow.append(LEFT);
+                } else {
+                    lineArrow.append(RIGHT);
+                }
+
+            }
+        } else {
+            if (event.getNodePosition() < 1) {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    lineArrow.append(LEFT);
+                } else {
+                    lineArrow.append(RIGHT);
+                }
+            } else {
+                if (MSG_FLOW.REQUEST.toString().equals(flow)) {
+                    lineArrow.append(RIGHT);
+                } else {
+                    lineArrow.append(LEFT);
+                }
+
+            }
+        }
+    }
 
 }
