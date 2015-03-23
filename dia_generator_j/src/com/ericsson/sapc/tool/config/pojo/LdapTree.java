9e790325a0de176afac680ebec9bd84003ce6382
kekenow <zhongke@live.com>
2015-03-23 21:57:26 +0800

    Add class LdapTree to construct a tree from ldif file

---
+++ dia_generator_j/src/com/ericsson/sapc/tool/config/pojo/LdapTree.java
@@ -0,0 +1,21 @@
+package com.ericsson.sapc.tool.config.pojo;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+
+public class LdapTree extends DefaultTreeModel {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 852670006354184487L;
+
+    public LdapTree(TreeNode root) {
+        super(root);
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+}
